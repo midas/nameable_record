110dda5193641d5981c3ad267ad203d63446e154
Jason Harrelson <jason@lookforwardenterprises.com>
2010-10-02 18:40:18 -0500

    Fix bug with incorrect module name.

--- lib/nameable_record/active_record_extensions.rb
+++ lib/nameable_record/active_record_extensions.rb
@@ -1,32 +1,32 @@
 module NameableRecord
   module ActiveRecordExtensions
-  def self.included( base )
-    base.extend ActsMethods
-  end
-
-  module ActsMethods
-    def has_name( *args )
-      unless included_modules.include? InstanceMethods
-        self.class_eval { extend ClassMethods }
-        include InstanceMethods
-      end
-      initialize_compositions( args )
+    def self.included( base )
+      base.extend ActsMethods
     end
 
-    alias_method :has_names, :has_name
-  end
+    module ActsMethods
+      def has_name( *args )
+        unless included_modules.include? InstanceMethods
+          self.class_eval { extend ClassMethods }
+          include InstanceMethods
+        end
+        initialize_compositions( args )
+      end
 
-  module ClassMethods
-    def initialize_compositions( attrs )
-      attrs.each do |attr|
-        composed_of attr, :class_name => "Nameable::Name", :mapping => [["#{attr}_last", "last"], ["#{attr}_first", "first"],
-          ["#{attr}_prefix", "prefix"], ["#{attr}_middle", "middle"],  ["#{attr}_suffix", "suffix"]]
+      alias_method :has_names, :has_name
+    end
+
+    module ClassMethods
+      def initialize_compositions( attrs )
+        attrs.each do |attr|
+          composed_of attr, :class_name => "NameableRecord::Name", :mapping => [["#{attr}_last", "last"], ["#{attr}_first", "first"],
+            ["#{attr}_prefix", "prefix"], ["#{attr}_middle", "middle"],  ["#{attr}_suffix", "suffix"]]
+        end
       end
     end
-  end
 
-  module InstanceMethods
+    module InstanceMethods
 
+    end
   end
-end
 end
